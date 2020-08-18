825dd36d785c16af859c61906c51fbf5d82842f2
Dzmitry Babak <alexgott1990@gmail.com>
2020-08-18 23:18:32 +0300

    add:tests

---
+++ store/test/test_serializer.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+from store.models import Book
+from store.serializers import BookSerializer
+
+
+class BookSerializerTestCase(TestCase):
+    def test_ok(self):
+        book_1 = Book.objects.create(name='Test book 1', price=215)
+        book_2 = Book.objects.create(name='Test book 2', price=225)
+        data = BookSerializer([book_1, book_2], many=True).data
+        expected_data = [
+            {
+                'id': book_1.id,
+                'name': 'Test book 1',
+                'price': '215.00'
+            },
+            {
+                'id': book_2.id,
+                'name': 'Test book 2',
+                'price': '225.00'
+            },
+
+        ]
+        self.assertEqual(expected_data,data)
