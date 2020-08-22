c93a123b8b0eaf3cd46fbe97e1139b5f81c9dac5
Dzmitry Babak <alexgott1990@gmail.com>
2020-08-22 03:55:32 +0300

    add: tests CRUD

--- store/test/test_serializer.py
+++ store/test/test_serializer.py
@@ -6,19 +6,23 @@ from store.serializers import BookSerializer
 
 class BookSerializerTestCase(TestCase):
     def test_ok(self):
-        book_1 = Book.objects.create(name='Test book 1', price=215)
-        book_2 = Book.objects.create(name='Test book 2', price=225)
+        book_1 = Book.objects.create(name='Test book 1', price=215,
+                                     author_name='Author 1')
+        book_2 = Book.objects.create(name='Test book 2', price=225,
+                                     author_name='Author 2')
         data = BookSerializer([book_1, book_2], many=True).data
         expected_data = [
             {
                 'id': book_1.id,
                 'name': 'Test book 1',
-                'price': '215.00'
+                'price': '215.00',
+                'author_name':'Author 1'
             },
             {
                 'id': book_2.id,
                 'name': 'Test book 2',
-                'price': '225.00'
+                'price': '225.00',
+                'author_name': 'Author 2'
             },
 
         ]
