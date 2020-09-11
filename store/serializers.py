4773e3e3798b0b44734bb5480c5196f8d8cb8f42
Dzmitry Babak <alexgott1990@gmail.com>
2020-09-11 21:10:03 +0300

    add: likes, rate and bookmark

--- store/serializers.py
+++ store/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework.serializers import ModelSerializer
 
-from store.models import Book
+from store.models import Book, UserBookRelation
 
 
 class BookSerializer(ModelSerializer):
     """Сериалайзер для книг"""
+
     class Meta:
         model = Book
         fields = '__all__'
+
+
+class UserBookRelationSerializer(ModelSerializer):
+    """Сериалайзер для статуса книг"""
+
+    class Meta:
+        model = UserBookRelation
+        fields = ('book', 'like', 'in_bookmarks', 'rate')
