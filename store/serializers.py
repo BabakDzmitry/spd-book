c8bc3b267531ac5fea9a115df3c94eeebbab9052
Dzmitry Babak <alexgott1990@gmail.com>
2020-09-30 16:40:00 +0300

    add:like

--- store/serializers.py
+++ store/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from store.models import Book, UserBookRelation
@@ -5,10 +6,14 @@ from store.models import Book, UserBookRelation
 
 class BookSerializer(ModelSerializer):
     """Сериалайзер для книг"""
+    likes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ('id', 'name', 'price', 'author_name')
+
+    def get_likes_count(self, instance):
+        return UserBookRelation.objects.filter(book=instance, like=True).count()
 
 
 class UserBookRelationSerializer(ModelSerializer):
